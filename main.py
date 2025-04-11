66b8a01b6a64166121f98e7a536589708cc31474
Fred Morrison <frederick.morrison@outlook.com>
2025-04-11 16:27:21 -0600

    put Google shared file link information into .env file

--- main.py
+++ main.py
@@ -3,32 +3,34 @@ import sys
 import requests
 from requests import Response
 
+from program_settings import ProgramSettings
+
 
 def get_python_version() -> str:
     return f'{sys.version_info.major}.{sys.version_info.minor}.{sys.version_info.micro}'
 
 
-def convert_google_drive_public_link_to_readable_url(link: str) -> str:
+def convert_google_drive_public_link_to_readable_url(google_file_id: str) -> str:
     """
     Converts a Google Drive share link for a file to one that can be used to read the file contents:
-    :param link: the original link
+    :param google_file_id: the original link
     :return: a URL string that can be used to read the file contents
 
     Example input:
     https://drive.google.com/file/d/1cU2M7HNdKLl8GXpXujgRwxBMGhlfmwHm/view?usp=sharing
     """
     find_start_of_id = 'file/d/'
-    start_index = link.index(find_start_of_id) + len(find_start_of_id)
-    ending_index = link.index('/view?')
-    id = link[start_index:ending_index]
+    start_index = google_file_id.index(find_start_of_id) + len(find_start_of_id)
+    ending_index = google_file_id.index('/view?')
+    file_id_extracted = google_file_id[start_index:ending_index]
     # print(f'{id = }')
-    return f'https://drive.google.com/uc?export=download&id={id}'
+    return f'https://drive.google.com/uc?export=download&id={file_id_extracted}'
 
 
 print(f'File I/O from URL using python version {get_python_version()}')
 
 # My Drive > public > python code snippets.txt
-google_share_link = 'https://drive.google.com/file/d/1cU2M7HNdKLl8GXpXujgRwxBMGhlfmwHm/view?usp=sharing'
+google_share_link: str = ProgramSettings.get_setting('GOOGLE_SHARED_TEXT_FILE_LINK')
 readable_url = convert_google_drive_public_link_to_readable_url(google_share_link)
 
 resp: Response = requests.get(readable_url)
