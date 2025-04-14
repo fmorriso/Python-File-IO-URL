bc1e7573c8d03c6ad2aa838eb3828f34ffd59e38
Fred Morrison <frederick.morrison@outlook.com>
2025-04-14 07:41:43 -0600

    update comments and add more type hints

--- README.md
+++ README.md
@@ -1,26 +1,32 @@
 # Python File I/O from URL
+
 Simple example of reading from a text file located on Google Drive and shared out to Viewers
 
 ## Directions for use
+
 If you want to point the code to your own Google Drive shared text file:
+
 1.  Open up the `.env` file with your favorite text editor.
 1.  Change the value of `GOOGLE_SHARED_TEXT_FILE_LINK` to point to your shared file located on your Google Drive.
 
 ## Tools Used
 
 | Tool          |  Version |
-|:--------------|---------:|
+| :------------ | -------: |
 | Python        |   3.13.2 |
 | python-dotenv |    1.1.0 |
+| requests      |   2.32.3 |
 | PyCharm       | 2024.3.5 |
 | VSCode        |   1.99.0 |
 
 ## Change History
 
-| Date       | Description      |
-|:-----------|:-----------------|
-| 2025-04-11 | Initial creation |
+| Date       | Description                                       |
+| :--------- | :------------------------------------------------ |
+| 2025-04-11 | Initial creation                                  |
+| 2025-04-15 | update requirements.txt to reflect requests level |
 
 ## References
-* [Read a file from Google Drive](https://fromzerotofullstack.com/courses/sheet2graph/ep11--reading-a-file-from-google-drive/)
-* [Python locale](https://docs.python.org/3/library/locale.html)
+
+- [Read a file from Google Drive](https://fromzerotofullstack.com/courses/sheet2graph/ep11--reading-a-file-from-google-drive/)
+- [Python locale](https://docs.python.org/3/library/locale.html)
--- main.py
+++ main.py
@@ -10,33 +10,43 @@ def get_python_version() -> str:
     return f'{sys.version_info.major}.{sys.version_info.minor}.{sys.version_info.micro}'
 
 
+def get_requests_version() -> str:
+    return requests.__version__
+
+
 def convert_google_drive_public_link_to_readable_url(google_shared_file_link: str) -> str:
     """
     Converts a Google Drive share link for a file to one that can be used to read the file contents:
     :param google_shared_file_link: the original link
     :return: a URL string that can be used to read the file contents
 
-    Example input:
+    Example input value:
     https://drive.google.com/file/d/1cU2M7HNdKLl8GXpXujgRwxBMGhlfmwHm/view?usp=sharing
+
+    Example return value:
+    'https://drive.google.com/uc?export=download&id=1cU2M7HNdKLl8GXpXujgRwxBMGhlfmwHm'
     """
     find_start_of_id = 'file/d/'
     start_index = google_shared_file_link.index(find_start_of_id) + len(find_start_of_id)
     ending_index = google_shared_file_link.index('/view?')
     file_id_extracted = google_shared_file_link[start_index:ending_index]
-    return f'https://drive.google.com/uc?export=download&id={file_id_extracted}'
+    usable_link: str = f'https://drive.google.com/uc?export=download&id={file_id_extracted}'
+    return usable_link
 
 
-print(f'File I/O from URL using python version {get_python_version()}')
+### MAIN LOGIC BEGINS HERE ###
+print(f'File I/O from URL using python version {get_python_version()} and requests version {get_requests_version()}')
 
 # My Drive > public > python code snippets.txt
-google_share_link: str = ProgramSettings.get_setting('GOOGLE_SHARED_TEXT_FILE_LINK')
-readable_url = convert_google_drive_public_link_to_readable_url(google_share_link)
+google_text_file_shared_link: str = ProgramSettings.get_setting('GOOGLE_SHARED_TEXT_FILE_LINK')
+readable_text_file_url = convert_google_drive_public_link_to_readable_url(google_text_file_shared_link)
 
-resp: Response = requests.get(readable_url)
-binary_string_content: bytes = resp.content  # binary string
+resp: Response = requests.get(readable_text_file_url)
+# the response content is considered a python binary string (e.g., b'some text')
+binary_string_content: bytes = resp.content  # binary string, a.k.a class bytes(Sequence[int])
 
 # now convert binary string to regular string
-string_content = binary_string_content.decode('utf-8')
+string_content: str = binary_string_content.decode('utf-8')
 
 # echo each line from the text file
 for line in string_content.splitlines():
