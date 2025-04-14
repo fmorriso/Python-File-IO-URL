235e8756fa9e20434c52bd9b84eefb77291b18e5
Fred Morrison <frederick.morrison@outlook.com>
2025-04-14 08:25:02 -0600

    create separate methods for "read all at once" vs. "stream one line at a time"

--- main.py
+++ main.py
@@ -34,20 +34,29 @@ def convert_google_drive_public_link_to_readable_url(google_shared_file_link: st
     return usable_link
 
 
-### MAIN LOGIC BEGINS HERE ###
-print(f'File I/O from URL using python version {get_python_version()} and requests version {get_requests_version()}')
+def read_file_contents_all_at_once():
+    # My Drive > public > python code snippets.txt
+    google_text_file_shared_link: str = ProgramSettings.get_setting('GOOGLE_SHARED_TEXT_FILE_LINK')
+    readable_text_file_url = convert_google_drive_public_link_to_readable_url(google_text_file_shared_link)
 
-# My Drive > public > python code snippets.txt
-google_text_file_shared_link: str = ProgramSettings.get_setting('GOOGLE_SHARED_TEXT_FILE_LINK')
-readable_text_file_url = convert_google_drive_public_link_to_readable_url(google_text_file_shared_link)
+    resp: Response = requests.get(readable_text_file_url)
+    # the response content is considered a python binary string (e.g., b'some text')
+    binary_string_content: bytes = resp.content  # binary string, a.k.a class bytes(Sequence[int])
 
-resp: Response = requests.get(readable_text_file_url)
-# the response content is considered a python binary string (e.g., b'some text')
-binary_string_content: bytes = resp.content  # binary string, a.k.a class bytes(Sequence[int])
+    # now convert binary string to regular string
+    string_content: str = binary_string_content.decode('utf-8')
 
-# now convert binary string to regular string
-string_content: str = binary_string_content.decode('utf-8')
+    # echo each line from the text file
+    for line in string_content.splitlines():
+        print(line)
 
-# echo each line from the text file
-for line in string_content.splitlines():
-    print(line)
+
+def main():
+    print(f'File I/O from URL using python version {get_python_version()}')
+    print(f'\tand requests version {get_requests_version()}')
+
+    read_file_contents_all_at_once()
+
+
+if __name__ == '__main__':
+    main()
